95ab01b729f6270c3172d6be456db2be9f41cbce
Juan Huamani <jgabrielhf100@gmail.com>
2023-06-27 00:26:36 -0500

    add-forms-login

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
-def login(request):
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redireccionar a la página de inicio o a la siguiente página deseada
+                return redirect('home')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'login/login.html', {'form': form})
 
-    return render(request, "login/login.html")
-
-def register(request):
+def register_view(request):
 
     return render(request, "register/register.html")
