f1087fcf1986f2eb0751242103cddc5ed0aed087
Juan Huamani <jgabrielhf100@gmail.com>
2023-07-04 14:52:07 -0500

    fix-gps

--- gps_tracking/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from tcp_server import start_tcp_server
-
-def start_server_view(request):
-    start_tcp_server()
-    return HttpResponse("TCP server started")
